3996a9664d6e7c5d05ba5d08b69e50cd607f7bc7
developermickey <mukesh.quazma@gmail.com>
2024-01-24 14:23:38 +0530

    DataTypes Value Added

---
+++ 01_Basic_JS/01_variables.js
@@ -0,0 +1,8 @@
+const backAccountNumber = 1234567890;
+let userName = "Mickey";
+var holderNumber = 12121212121;
+
+// Const Value not be changed
+// Var not be use
+// Scope
+console.table([backAccountNumber, userName, holderNumber]);
---
+++ 01_Basic_JS/02_dataTypes.js
@@ -0,0 +1,13 @@
+let userName = "developermickey"; // String Type
+let userAge = 20; // Number Type
+let userIsLogined = true; // boolean Type
+let whoIsWinner = NaN; // Number Type
+let userScore = undefined; // undefined Type
+
+// how can Identify any variable tyeps
+
+console.log(typeof userName); // String Type
+console.log(typeof userAge); // String Type
+console.log(typeof userIsLogined); // String Type
+console.log(typeof whoIsWinner); // String Type
+console.log(typeof userScore); // String Type
